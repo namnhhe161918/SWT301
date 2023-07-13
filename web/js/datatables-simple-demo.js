a1b60044615b01e55e4619159a6c0f4289bc883b
haonqhe161800 <haonqhe161800@fpt.edu.vn>
2023-07-14 00:42:59 +0700

    fixweb/js/datatables-simple-demo.js

--- web/js/datatables-simple-demo.js
+++ web/js/datatables-simple-demo.js
@@ -1,13 +1,11 @@
 window.addEventListener('DOMContentLoaded', event => {
     if (typeof simpleDatatables !== 'undefined') {
         const datatablesSimple = document.getElementById('datatablesSimple');
-        let myDataTable = null;
         
         if (datatablesSimple) {
-            myDataTable = new simpleDatatables.DataTable(datatablesSimple);
+            new simpleDatatables.DataTable(datatablesSimple);
         }
         
-        // Sử dụng myDataTable cho các hoạt động liên quan đến bảng dữ liệu
-        // Ví dụ: myDataTable.configure({ ... }), myDataTable.destroy(), myDataTable.update(), v.v.
+        // Xóa phần gán giá trị không sử dụng
     }
 });
