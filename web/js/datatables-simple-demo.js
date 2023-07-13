bab25978e49bd8977c67238e97cc57a7858f2eba
haonqhe161800 <haonqhe161800@fpt.edu.vn>
2023-07-14 00:46:09 +0700

    nhucudeothemsuanua

--- web/js/datatables-simple-demo.js
+++ web/js/datatables-simple-demo.js
@@ -1,11 +1,9 @@
 window.addEventListener('DOMContentLoaded', event => {
-    if (typeof simpleDatatables !== 'undefined') {
-        const datatablesSimple = document.getElementById('datatablesSimple');
-        
-        if (datatablesSimple) {
-            new simpleDatatables.DataTable(datatablesSimple);
-        }
-        
-        // Xóa phần gán giá trị không sử dụng
+    // Simple-DataTables
+    // https://github.com/fiduswriter/Simple-DataTables/wiki
+
+    const datatablesSimple = document.getElementById('datatablesSimple');
+    if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
     }
 });
