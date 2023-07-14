899b23745076a6ccc143b40743b0443f4f8d6b42
tungnhhe161576 <tungnhhe161576@fpt.edu.vn>
2023-07-14 13:19:36 +0700

    error 3

--- src/java/test/com/controllers/User.java
+++ src/java/test/com/controllers/User.java
@@ -98,7 +98,6 @@ public class User extends HttpServlet {
 
     private Random random = new Random();
     public String generateDisplayName() {
-//        Random random = new Random();
         String displayName = "@displayname";
         StringBuilder sb = new StringBuilder(10);
         String sequenceChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
