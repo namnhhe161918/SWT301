c6156468aabb6d2eff6da15e806bcf76a9843e34
namnhhe161918 <102135283+namnhhe161918@users.noreply.github.com>
2023-07-14 13:47:53 +0700

    Update DAOUser.java

--- src/java/test/com/daos/DAOUser.java
+++ src/java/test/com/daos/DAOUser.java
@@ -15,14 +15,12 @@ import test.com.modules.DBConnect;
 public class DAOUser extends DBConnect {
 
     //check emailInput & password
-    public AccountUser checkLogin(String emailInput, String password) {
-
-        String sql = "SELECT * FROM User_type WHERE email = ? AND password = ?";
-        try {
-            PreparedStatement st = conn.prepareStatement(sql);
-            st.setString(1, emailInput);
-            st.setString(2, password);
-            ResultSet rs = st.executeQuery();
+public AccountUser checkLogin(String emailInput, String password) {
+    String sql = "SELECT * FROM User_type WHERE email = ? AND password = ?";
+    try (PreparedStatement st = conn.prepareStatement(sql)) {
+        st.setString(1, emailInput);
+        st.setString(2, password);
+        try (ResultSet rs = st.executeQuery()) {
             if (rs.next()) {
                 AccountUser au = new AccountUser(
                         rs.getInt("user_id"),
@@ -42,21 +40,27 @@ public class DAOUser extends DBConnect {
                 );
                 return au;
             }
-        } catch (SQLException e) {
-            System.out.println(e);
         }
-        return null;
+    } catch (SQLException e) {
+        System.out.println(e);
+    } finally {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
     }
+    return null;
+}
 
     //check exist
-    public AccountUser checkExist(String emailInput) {
-        String sql = "SELECT * FROM User_type WHERE email = ?";
-
-        try {
-            PreparedStatement st = conn.prepareStatement(sql);
-            st.setString(1, emailInput);
-            ResultSet rs = st.executeQuery();
-
+public AccountUser checkExist(String emailInput) {
+    String sql = "SELECT * FROM User_type WHERE email = ?";
+    try (PreparedStatement st = conn.prepareStatement(sql)) {
+        st.setString(1, emailInput);
+        try (ResultSet rs = st.executeQuery()) {
             if (rs.next()) {
                 AccountUser au = new AccountUser(
                         rs.getInt("user_id"),
@@ -76,11 +80,13 @@ public class DAOUser extends DBConnect {
                 );
                 return au;
             }
-        } catch (SQLException e) {
-            System.out.println(e);
         }
-        return null;
+    } catch (SQLException e) {
+        System.out.println(e);
     }
+    return null;
+}
+
 
     //INSERT AccountUser
     public void insertUser(String emailInput, String password, String fullname, String displayname, String dob, int gender, int roleid) {
@@ -93,9 +99,10 @@ public class DAOUser extends DBConnect {
                 + "           ,[gender]\n"
                 + "           ,[role_id])\n"
                 + "     VALUES (?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement st = null;
 
         try {
-            PreparedStatement st = conn.prepareStatement(sql);
+             st = conn.prepareStatement(sql);
             st.setString(1, emailInput);
             st.setString(2, password);
             st.setString(3, fullname);
@@ -106,115 +113,188 @@ public class DAOUser extends DBConnect {
             st.executeUpdate();
         } catch (SQLException e) {
             System.out.println(e);
+        }  finally {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
         }
+      }
     }
+
+
+
 
     //CHECK EMAIL
-    public boolean checkEmail(String emailInput) {
-
-        String sql = "SELECT * FROM [User_type] WHERE email = ?";
-        try {
-            PreparedStatement st = conn.prepareCall(sql);
-            st.setString(1, emailInput);
-            ResultSet rs = st.executeQuery();
-            if (rs.next()) {
-                return true;
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
+public boolean checkEmail(String emailInput) {
+    String sql = "SELECT * FROM [User_type] WHERE email = ?";
+    PreparedStatement st = null;
+    ResultSet rs = null;
+    try {
+        st = conn.prepareCall(sql);
+        st.setString(1, emailInput);
+        rs = st.executeQuery();
+        if (rs.next()) {
+            return true;
         }
-        return false;
+    } catch (SQLException e) {
+        System.out.println(e);
+    } finally {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
     }
+    return false;
+}
+
+
 
     //update password
-    public void updatePasswordByName(String pass,String emailInput) {
-        String sql = "  UPDATE [dbo].[User_type]\n"
-                + "   SET [password] = ? \n"
-                + " WHERE [email] = ?";
-        
-        try {
-            PreparedStatement st = conn.prepareCall(sql);
-            st.setString(1,pass);
-            st.setString(2,emailInput);
-            st.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e);
+ public void updatePasswordByName(String pass, String emailInput) {
+    String sql = "UPDATE [dbo].[User_type]\n"
+            + "SET [password] = ? \n"
+            + "WHERE [email] = ?";
+    PreparedStatement st = null;
+    try {
+        st = conn.prepareCall(sql);
+        st.setString(1, pass);
+        st.setString(2, emailInput);
+        st.executeUpdate();
+    } catch (SQLException e) {
+        System.out.println(e);
+    } finally {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
         }
-
     }
+}
+
     
      //update profile(fullname,describle yourself, image, display_name,address,modifiy date,dob, academic level, gender)
-    public void updateProfile(int id , String descyouself, String fullname, String image, String displayname, String address, String dob, String academiclevel, String modifydate, int gender) {
-        String sql = "UPDATE [dbo].[User_type]\n"
-                + "   SET [describe_yourself] = ?\n"
-                + "      ,[fullname] = ?\n"
-                + "      ,[image] = ?\n"
-                + "      ,[display_name] = ?\n"
-                + "      ,[address] = ?\n"
-                + "      ,[date_of_birth] = ?\n"
-                + "      ,[academic_level] = ?\n"
-                + "      ,[modify_date] = ?\n"
-                + "      ,[gender] = ?\n"
-                + " WHERE user_id = ?";
-
-        try {
-            PreparedStatement st = conn.prepareCall(sql);
-            st.setString(1, descyouself);
-            st.setString(2, fullname);
-            st.setString(3, image);
-            st.setString(4, displayname);
-            st.setString(5, address);
-            st.setString(6, dob);
-            st.setString(7, academiclevel);
-            st.setString(8, modifydate);
-            st.setInt(9, gender);
-            st.setInt(10, id);
-            st.executeUpdate();
-                    st.close();
-        } catch (SQLException e) {
-            System.out.println(e);
-        } 
-    }
-    
-    public Vector<AccountUser> getAll(String sql) {
-        Vector<AccountUser> vector = new Vector<AccountUser>();
-        ResultSet rs = this.getData(sql);
-        try {
-            while (rs.next()) {
-                int user_id = rs.getInt(1);
-                String email = rs.getString(2);
-                String password = rs.getString(3);
-                String descyourself = rs.getString(4);
-                String fullName = rs.getString(5);
-                String image = rs.getString(6);
-                String display_name = rs.getString(7);
-                String created_date = rs.getString(8);
-                String address = rs.getString(9);
-                String dob = rs.getString(10);
-                String academiclevel = rs.getString(11);
-                String modify_date = rs.getString(12);
-                int gender = rs.getInt(13);
-                int role_id = rs.getInt(14);
-                AccountUser au = new AccountUser(user_id, email, password, descyourself, fullName, image, display_name, created_date, address, dob, academiclevel, modify_date, gender, role_id);
-                vector.add(au);
+public void updateProfile(int id, String descyourself, String fullname, String image, String displayname, String address, String dob, String academiclevel, String modifydate, int gender) {
+    String sql = "UPDATE [dbo].[User_type]\n"
+            + "SET [describe_yourself] = ?\n"
+            + "   ,[fullname] = ?\n"
+            + "   ,[image] = ?\n"
+            + "   ,[display_name] = ?\n"
+            + "   ,[address] = ?\n"
+            + "   ,[date_of_birth] = ?\n"
+            + "   ,[academic_level] = ?\n"
+            + "   ,[modify_date] = ?\n"
+            + "   ,[gender] = ?\n"
+            + "WHERE user_id = ?";
+    PreparedStatement st = null;
+    try {
+        st = conn.prepareCall(sql);
+        st.setString(1, descyourself);
+        st.setString(2, fullname);
+        st.setString(3, image);
+        st.setString(4, displayname);
+        st.setString(5, address);
+        st.setString(6, dob);
+        st.setString(7, academiclevel);
+        st.setString(8, modifydate);
+        st.setInt(9, gender);
+        st.setInt(10, id);
+        st.executeUpdate();
+    } catch (SQLException e) {
+        System.out.println(e);
+    } finally {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                System.out.println(e);
             }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
         }
-        return vector;
     }
-  //deleteUser
-    public void deleteUser(int userId) {
-        String sql = "DELETE FROM [User_type] WHERE user_id = ?";
+}
 
-        try {
-            PreparedStatement st = conn.prepareStatement(sql);
-            st.setInt(1, userId);
-            st.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e);
+    
+public Vector<AccountUser> getAll(String sql) {
+    Vector<AccountUser> vector = new Vector<AccountUser>();
+    ResultSet rs = null;
+    PreparedStatement st = null;
+    try {
+        rs = this.getData(sql);
+        st = rs.getStatement();
+        while (rs.next()) {
+            int user_id = rs.getInt(1);
+            String email = rs.getString(2);
+            String password = rs.getString(3);
+            String descyourself = rs.getString(4);
+            String fullName = rs.getString(5);
+            String image = rs.getString(6);
+            String display_name = rs.getString(7);
+            String created_date = rs.getString(8);
+            String address = rs.getString(9);
+            String dob = rs.getString(10);
+            String academiclevel = rs.getString(11);
+            String modify_date = rs.getString(12);
+            int gender = rs.getInt(13);
+            int role_id = rs.getInt(14);
+            AccountUser au = new AccountUser(user_id, email, password, descyourself, fullName, image, display_name, created_date, address, dob, academiclevel, modify_date, gender, role_id);
+            vector.add(au);
+        }
+    } catch (SQLException ex) {
+        ex.printStackTrace();
+    } finally {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
+    return vector;
+}
+
+  //deleteUser
+  public void deleteUser(int userId) {
+    String sql = "DELETE FROM [User_type] WHERE user_id = ?";
+    PreparedStatement st = null;
+    try {
+        st = conn.prepareStatement(sql);
+        st.setInt(1, userId);
+        st.executeUpdate();
+    } catch (SQLException e) {
+        System.out.println(e);
+    } finally {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
 
     public static void main(String[] args) {
         DAOUser dudb = new DAOUser();
